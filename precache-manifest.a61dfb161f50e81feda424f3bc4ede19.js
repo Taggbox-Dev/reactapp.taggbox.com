897e139be3cc4fa753bab90f3974f00d6ca95291
sudarshan bairagi <sudarshan.bairagi@taggbox.com>
2023-04-05 16:44:53 +0530

    update

---
+++ precache-manifest.a61dfb161f50e81feda424f3bc4ede19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad47ed4ba024a793b6e7b76f60a70b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "981161a17630d0c31ab6",
+    "url": "./static/css/main.2010550e.chunk.css"
+  },
+  {
+    "revision": "21ab79d4f38158f33a3e",
+    "url": "./static/js/2.9a68a94a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "./static/js/2.9a68a94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981161a17630d0c31ab6",
+    "url": "./static/js/main.43fc53c7.chunk.js"
+  },
+  {
+    "revision": "4c009b4c7636f6f58364",
+    "url": "./static/js/runtime-main.ae5b75c7.js"
+  },
+  {
+    "revision": "96020a42815a9e292363631b63f0d47a",
+    "url": "./static/media/pinterest.96020a42.svg"
+  }
+]);
